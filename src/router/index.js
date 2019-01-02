a949c77671213686d450b75b46b79ff52e858059
Kim Do Ky <makingfunk0@gmail.com>
2019-01-02 22:21:08 +0900

    Vue.js and Vuex

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Backlog from '@/components/Backlog';
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/backlog',
+      component: Backlog,
+    },
+    {
+      path: '*',
+      redirect: '/backlog',
+    },
     {
       path: '/',
       name: 'Default',
