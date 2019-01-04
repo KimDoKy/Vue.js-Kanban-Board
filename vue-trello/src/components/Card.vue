2b2747818584ac8f752609e83d8a1fa9be8896f2
Kim Do Ky <makingfunk0@gmail.com>
2019-01-04 21:00:46 +0900

    데이터 불러오기

--- vue-trello/src/components/Card.vue
+++ vue-trello/src/components/Card.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     Card
-    <div>cid: {{cid}}</div>
+    <div v-if="loading">loading card...</div>
+    <div v-else>cid: {{cid}}</div>
   </div>
 </template>
 
@@ -9,16 +10,24 @@
 export default {
   data() {
     return {
-      cid: 0
+      cid: 0,
+      loading: false
     }
   },
   watch: {
-    '$route'() {
-      this.cid = this.$route.params.cid
+    '$route': {
+      handler: 'fatchData',
+      immediate: true
     }
   },
-  created() {
-    this.cid =this.$route.params.cid
+  methods: {
+    fatchData() {
+      this.loading = true
+      setTimeout(() => {
+        this.cid =this.$route.params.cid
+        this.loading = false
+      }, 500)
+    }
   }
 }
 </script>
