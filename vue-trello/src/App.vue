2b3c843894e8ddd0bdc558140b50a6ba8e0b7bb2
Kim Do Ky <makingfunk0@gmail.com>
2019-01-04 20:00:34 +0900

    라우터 링크

--- vue-trello/src/App.vue
+++ vue-trello/src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div id="app">
-    여기서부터 시작합니다.
+    <Navbar />
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar.vue'
+
 export default {
   name: 'app',
+  components: { Navbar },
   data () {
     return {
-      'msg': '여기서부터 시작'
     }
   }
 }
