6ebe71cf4f5d07ffef4ea86a2f2686ead7a6c6f8
Kim Do Ky <makingfunk0@gmail.com>
2019-01-06 16:18:22 +0900

    로그인

--- vue-trello/src/api/index.js
+++ vue-trello/src/api/index.js
@@ -4,24 +4,34 @@ import router from '../router'
 const DOMAIN = 'http://localhost:3000'
 const UNAUTHORIZED = 401
 const onUnauthorized = () => {
-  router.push('/login')
+  router.push(`/login?rPath=${encodeURIComponent(location.pathname)}`)
 }
 
 const request = (method, url, data) => {
-  return axios ({
+  return axios({
     method,
     url: DOMAIN + url,
     data
   }).then(result => result.data)
     .catch(result => {
       const {status} = result.response
-      if (status === UNAUTHORIZED) return onUnauthorized()
-      throw Error(result)
+      if (status === UNAUTHORIZED) onUnauthorized()
+      throw result.response
     })
 }
 
+export const setAuthInHeader = token => {
+  axios.defaults.headers.common['Authorization'] = token ? `Bearer ${token}` : null;
+}
+
 export const board = {
-  fatch() {
+  fetch() {
     return request('get', '/boards')
+  }
+}
+
+export const auth = {
+  login(email, password) {
+    return request('post', '/login', {email, password})
   }
 }
