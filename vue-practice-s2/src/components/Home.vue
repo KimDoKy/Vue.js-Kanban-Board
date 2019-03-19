a5ce2bcf7c1aeb41e706494b9e5451e09a544bbc
Kim Do Ky <makingfunk0@gmail.com>
2019-03-19 21:16:24 +0900

    axios

--- vue-practice-s2/src/components/Home.vue
+++ vue-practice-s2/src/components/Home.vue
@@ -3,10 +3,9 @@
     Home
     <div>
       Board List:
-      <div v-if="loading">Loading...</div>
-      <div v-else>
-        Api result: {{apiRes}}
-      </div>
+      <div v-if='loading'>Loading ...</div>
+      <div v-else>Api result: <pre>{{apiRes}}</pre></div>
+      <div v-if='error'><pre>{{error}}</pre></div>
       <ul>
         <li>
           <router-link to='/b/1'>Board 1</router-link>
@@ -20,11 +19,14 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   data() {
     return {
       loading: false,
-      apiRes: ''
+      apiRes: '',
+      error: ''
     }
   },
   created() {
@@ -33,17 +35,16 @@ export default {
   methods: {
     fetchData() {
       this.loading = true
-      const req = new XMLHttpRequest()
-      req.open('GET', 'http://localhost:3000/health')
-      req.send()
-      req.addEventListener('load', () => {
-        this.loading = false
-        this.apiRes = {
-          status: req.status,
-          statueText: req.statusText,
-          response: JSON.parse(req.response)
-        }
-      })
+      axios.get('http://localhost:3000/health')
+        .then(res => {
+          this.apiRes = res.data
+        })
+        .catch(res => {
+          this.error = res.response.data
+        })
+        .finally(() => {
+          this.loading = false
+        })
     }
   }
 }
