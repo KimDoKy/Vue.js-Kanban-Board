ba029fce8ae4832155ca997364b51882810fc865
Kim Do Ky <makingfunk0@gmail.com>
2019-03-20 00:35:00 +0900

    use axios on api

--- vue-practice-s2/src/components/Home.vue
+++ vue-practice-s2/src/components/Home.vue
@@ -22,7 +22,7 @@
 </template>
 
 <script>
-import axios from 'axios'
+import {board} from '../api'
 
 export default {
   data() {
@@ -37,12 +37,9 @@ export default {
   methods: {
     fetchData() {
       this.loading = true
-      axios.get('http://localhost:3000/boards')
-        .then(res => {
-          this.boards = res.data
-        })
-        .catch(res => {
-          this.$router.replace('/login')
+      board.fetch()
+        .then(data => {
+          this.boards = data
         })
         .finally(() => {
           this.loading = false
